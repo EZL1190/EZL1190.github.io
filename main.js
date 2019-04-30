181e70fb98fc82c5061f44ea23956f28d81fb48e
unknown <e.z.l@live.com>
2019-04-30 09:45:03 +0200

    fetch

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert("ads");
+async function login2() {
+
+        const response = await fetch("https://www.holdsport.dk/graphql", {
+                method: "OPTIONS",
+                headers: { 
+                    "Content-Type": "application/json",
+                    "Authorization": "1411871:rZvkWvqN1E-eq6EZ-NVJug:156823",
+                    "Access-Control-Allow-Origin": "*" 
+                },
+                body: JSON.stringify({ "query": "current_user{name}" }),
+        })
+
+        const data = await response.text()
+        console.log(data);
+}
+
+login2();
